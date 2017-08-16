9eac85cf9a1a3ccfbae53124212e5d15e8ae8722
ITECO\akhmetzyanov <akhmetzyanov@i-teco.ru>
2017-08-16 16:09:04 +0300

    Task complete with hardcoded values of color in selector component

--- App.jsx
+++ App.jsx
@@ -8,44 +8,26 @@ class ColorSelector extends React.Component{
     super(props);
 
     this.state = {
-      var1: 0,
-      var2: 0,
-      result: 0
+      colorName: '',
     };
 
-
-    this.textLabels = {
-      expres: "Expression",
-      operator: "OP",
-      equal: "=",
-      add: "+",
-      sub: "-",
-      mult: "*"
-    };
-
-    // this.getVar1Handler = this.getVar1Handler.bind(this);
-
+    this.handlerColSel = this.handlerColSel.bind(this);
   }
 
-  divStyle : {
-    display: "inline-block",
+  handlerColSel(e){
+    console.log("Color: " + e.target.value);
 
+    this.setState({
+      colorName: e.target.value
+    });
   }
-
-  handlerColSel(evnt){
-    console.log("Handler color");
-  }
-
-
 
   render(){
     return(
-      <div style={this.divStyle}>
+      <div>
 
           <Selector onClick={this.handlerColSel} />
-          <textarea className="textarea">ia quam venenatis vestibulum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</textarea>
-
-
+          <ColoredText color={this.state.colorName} />
 
       </div>
     );
@@ -55,32 +37,64 @@ class ColorSelector extends React.Component{
 
 
 
+
+class ColoredText extends React.Component {
+    constructor(props) {
+      super(props);
+
+    }
+
+    render(){
+
+      var textNewStyle = {
+        color: this.props.color,
+      };
+
+      return(
+        <div>
+          <textarea style={textNewStyle}>This is textarea </textarea>
+        </div>
+      );
+    }
+
+}
+
+
+
 class Selector extends React.Component {
   constructor(props) {
     super(props);
 
+    this.textLabels = {
+        red: "Red",
+        green: "Green",
+        blue: "Blue"
+    };
+
+    this.colorsVal = {
+      red: "red",
+      green: "green",
+      blue: "blue",
+    };
   }
 
 
 
-
-
-
   render(){
-
-
-
     return(
-      <div className="div-color-sel" >
+      <div>
           <div>
-            <input id="red" className="color-sel" type="radio" name="color-group" value="Red"  onClick={this.props.onClick}></input>
-            <label for="red">Red</label>
+            <input id="red" className="color-sel" type="radio" name="color-group" value={this.colorsVal.red} onClick={this.props.onClick}></input>
+            <label for="red">{this.textLabels.red}</label>
           </div>
           <div>
-            <input  id="green" className="color-sel" type="radio"  name="color-group" value="Green" onClick={this.props.onClick}></input>
-            <label for="green">Green</label>
+            <input  id="green" className="color-sel" type="radio"  name="color-group" value={this.colorsVal.green} onClick={this.props.onClick}></input>
+            <label for="green">{this.textLabels.green}</label>
           </div>
-
+          <div>
+            <input  id="blue" className="color-sel" type="radio"  name="color-group" value={this.colorsVal.blue} onClick={this.props.onClick}></input>
+            <label for="blue">{this.textLabels.blue}</label>
+          </div>
       </div>
     );
   }
